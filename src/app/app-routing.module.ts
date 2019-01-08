43cadf2583c2067279b2dc176dc22f65d2852916
Sergio Martín <giosoftware@gmail.com>
2019-01-08 10:14:40 +0100

    Paths were added to general routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './commons/home/home.component';
+import { NotfoundComponent } from './commons/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', loadChildren: './members/members.module#MembersModule'},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
